16e4bf2a73f23ded85b84ab9cd184661dadfd88b
pushpak261 <pushpakrajendra@gmail.com>
2025-03-13 10:17:13 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 Master Branch
+=======
+Controllers & Api's Testing
+>>>>>>> a15d079 (Initial commit)
---
+++ src/main/java/com/ecommerce/sportshub/repository/ProductRepo.java
@@ -0,0 +1,14 @@
+package com.ecommerce.sportshub.repository;
+
+import com.ecommerce.sportshub.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepo extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryId(Long categoryId);
+    List<Product> findByNameContainingOrDescriptionContaining(String name, String description);
+
+    //modified
+
+}
